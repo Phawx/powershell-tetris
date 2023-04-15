d7737132c908c50337e09209f42ea7f1ab8f2885
Cary Golomb <carygolomb@gmail.com>
2023-04-15 19:34:03 -0400

    first implementation of shapes

--- tetris.ps1
+++ tetris.ps1
@@ -1,11 +1,9 @@
-function Show-Game {
+function Update-Game {
     param(
         $board,
         $tetromino
     )
-
-    cls
-    $updatedBoard = $board | % { ,@( $_ ) }
+    $updatedBoard = Copy-Board -board $board
     $tetromino | % {
         $y = $_.Y + $yOffset
         $x = $_.X + $xOffset
@@ -14,17 +12,61 @@ function Show-Game {
     $updatedBoard | % { ($_ | % { if ($_ -eq 1) { "*" } else { " " } }) -join "" }
 }
 
+function Copy-Board {
+    param (
+        $board
+    )
+
+    $newBoard = New-Object 'System.Collections.Generic.List[Object]'
+    foreach ($row in $board) {
+        $newRow = $row.Clone()
+        $newBoard.Add($newRow)
+    }
+
+    return $newBoard
+}
+
+function Test-Collision {
+    param(
+        $board,
+        $tetromino,
+        $xOffset,
+        $yOffset
+    )
+
+    foreach ($block in $tetromino) {
+        $x = $block.X + $xOffset
+        $y = $block.Y + $yOffset
+        if ($x -lt 0 -or $x -ge $boardWidth -or $y -ge $boardHeight -or ($y -ge 0 -and $board[$y][$x] -eq 1)) {
+            return $true
+        }
+    }
+
+    return $false
+}
 
 
 
 
 $boardWidth = 10
 $boardHeight = 20
-$board = @(,0 * $boardWidth) * $boardHeight
+$board = New-Object 'System.Collections.Generic.List[Object]'
+for ($i = 0; $i -lt $boardHeight; $i++) {
+    $row = ,@(0) * $boardWidth
+    $board.Add($row)
+}
+
 
 $tetrominoes = @(
-    @([PSCustomObject]@{X=0;Y=0},[PSCustomObject]@{X=1;Y=0},[PSCustomObject]@{X=0;Y=1},[PSCustomObject]@{X=1;Y=1})
+    @([PSCustomObject]@{X=0;Y=0},[PSCustomObject]@{X=1;Y=0},[PSCustomObject]@{X=0;Y=1},[PSCustomObject]@{X=1;Y=1}), # O shape
+    @([PSCustomObject]@{X=0;Y=0},[PSCustomObject]@{X=1;Y=0},[PSCustomObject]@{X=2;Y=0},[PSCustomObject]@{X=3;Y=0}), # I shape
+    @([PSCustomObject]@{X=1;Y=0},[PSCustomObject]@{X=0;Y=1},[PSCustomObject]@{X=1;Y=1},[PSCustomObject]@{X=2;Y=1}), # T shape
+    @([PSCustomObject]@{X=0;Y=0},[PSCustomObject]@{X=1;Y=0},[PSCustomObject]@{X=1;Y=1},[PSCustomObject]@{X=2;Y=1}), # Z shape
+    @([PSCustomObject]@{X=1;Y=0},[PSCustomObject]@{X=2;Y=0},[PSCustomObject]@{X=0;Y=1},[PSCustomObject]@{X=1;Y=1}), # S shape
+    @([PSCustomObject]@{X=0;Y=0},[PSCustomObject]@{X=1;Y=0},[PSCustomObject]@{X=2;Y=0},[PSCustomObject]@{X=2;Y=1}), # L shape
+    @([PSCustomObject]@{X=2;Y=0},[PSCustomObject]@{X=0;Y=1},[PSCustomObject]@{X=1;Y=1},[PSCustomObject]@{X=2;Y=1})  # J shape
 )
+
 
 $currentTetrominoIndex = Get-Random -Minimum 0 -Maximum $tetrominoes.Count
 $tetromino = $tetrominoes[$currentTetrominoIndex]
@@ -39,12 +81,28 @@ while ($true) {
     }
 
     switch ($key.VirtualKeyCode) {
-        37 { $xOffset -= 1 }
-        39 { $xOffset += 1 }
-        40 { $yOffset += 1 }
+        37 {
+            if (-not (Test-Collision -board $board -tetromino $tetromino -xOffset ($xOffset - 1) -yOffset $yOffset)) {
+                $xOffset -= 1
+            }
+        }
+        39 {
+            if (-not (Test-Collision -board $board -tetromino $tetromino -xOffset ($xOffset + 1) -yOffset $yOffset)) {
+                $xOffset += 1
+            }
+        }
+        40 {
+            if (-not (Test-Collision -board $board -tetromino $tetromino -xOffset $xOffset -yOffset ($yOffset + 1))) {
+                $yOffset += 1
+            }
+        }
     }
 
-    $yOffset += 1
-    Show-Game -board $board -tetromino $tetromino
+    if (-not (Test-Collision -board $board -tetromino $tetromino -xOffset $xOffset -yOffset ($yOffset + 1))) {
+        $yOffset += 1
+    }
+
+    Update-Game -board $board -tetromino $tetromino
     Start-Sleep -Milliseconds 500
 }
+
